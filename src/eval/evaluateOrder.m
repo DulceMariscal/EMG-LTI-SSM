fbb2177ce2774ddab5b7f41604f63d87112879f5
Unknown <itu.pablo@gmail.com>
2019-06-16 22:05:30 -0400

    New evaluate order script and block-wise fitting script

--- src/eval/evaluateOrder.m
+++ src/eval/evaluateOrder.m
@@ -1,90 +1,123 @@
 addpath(genpath('../../../matlab-linsys/'))
 addpath(genpath('../../../robustCov/'))
 addpath(genpath('../../'))
-%% First: load models fitted w/all data, look at BIC, AIC, LRT
-load ../../res/allDataModels.mat model datSet
-model=cellfun(@(x) x.canonize('canonical'),model,'UniformOutput',false);
-vizDataLikelihood(model,datSet)
-ph=findobj(gcf,'Type','Axes');
-for i=1:length(ph)
-  set(ph(i),'XTickLabelRotation',0,'XTickLabel',mat2cell(num2str([0:10]'),ones(11,1),2))
-end
-datSet.vizFit(model(2:5))
-%% Second: load models trained on odd/even, look at cross-val logL
-load('../../res/oddEvenCV_new.mat') %This dataset has the first stride of
-%both adapt and post in the odd-fold.
-%load('../../res/oddEvenCV2.mat') %These datasets contain the first stride 
-%of adapt in the odd-fold, and the first of post in the even-fold
-
-f1=vizDataLikelihood(fitMdlOE(:,1),datSetOE);
-ph=findobj(gcf,'Type','Axes');
-f2=vizDataLikelihood(fitMdlOE(:,2),datSetOE);
-ph1=findobj(gcf,'Type','Axes');
-
-fh=figure;
-ah=copyobj(ph([2,3]),fh);
-ah(1).Title.String={'Odd-model';'Cross-validation'};
-ah(1).YAxis.Label.String={'Even-data'; 'log-L'};
-ah(2).Title.String={'Odd-model';'-BIC/2'};
-ah(2).XTickLabel={'1','2','3','4','5','6'};
-ah(1).XTickLabel={'1','2','3','4','5','6'};
-ah1=copyobj(ph1([1,4]),fh);
-ah1(2).Title.String={'Even-model';'Cross-validation'};
-ah1(2).YAxis.Label.String={'Odd-data';'log-L'};
-ah1(2).XAxis.Label.String={'Model Order'};
-ah1(2).XTickLabel={'1','2','3','4','5','6'};
-ah1(1).XAxis.Label.String={'Model Order'};
-ah1(1).XTickLabel={'1','2','3','4','5','6'};
-ah1(1).Title.String={'Even-model';'-BIC/2'};
-set(gcf,'Name','Odd/even cross-validation');
-
-
-%% Third: load models trained on adapt/post, look at cross-val
-%This dataset includes subject 16
-% clear all
-% load('../../res/adaptPostCVw16.mat')
-% %Adapt data-trained, eval on post
-% vizDataLikelihood(model(:,1),datSet)
-% set(gcf,'Name','Adapt-data trained models');
-% ph=findobj(gcf,'Type','Axes');
-% ph(end).YAxis.Label.String='Adapt data';
-% ph(end-3).YAxis.Label.String='Post data';
-% %The opposite
-% vizDataLikelihood(model(1:6,2),datSet) %The script failed before fitting the order 6 model, thus, we just 
-% set(gcf,'Name','Post-data trained models');
-% ph=findobj(gcf,'Type','Axes');
-% ph(end).YAxis.Label.String='Adapt data';
-% ph(end-3).YAxis.Label.String='Post data';
-%% Fourth: load models trained on adapt/post, look at cross-val
+%% Load models
 clear all
-load('../../res/adaptPostCV.mat')
-f1=vizDataLikelihood(model(:,1),datSet);
-ph=findobj(gcf,'Type','Axes');
-f2=vizDataLikelihood(model(:,2),datSet);
-ph1=findobj(gcf,'Type','Axes');
-
-fh=figure;
-ah=copyobj(ph([2,3]),fh);
-ah(1).Title.String={'Adapt-model';'Cross-validation'};
-ah(1).YAxis.Label.String={'Post-data'; 'log-L'};
-ah(2).Title.String={'Adapt-model';'-BIC/2'};
-ah(2).XTickLabel={'0','1','2','3','4','5','6'};
-ah(1).XTickLabel={'0','1','2','3','4','5','6'};
-ah1=copyobj(ph1([1,4]),fh);
-ah1(2).Title.String={'Post-model';'Cross-validation'};
-ah1(2).YAxis.Label.String={'Adapt-data';'log-L'};
-ah1(2).XAxis.Label.String={'Model Order'};
-ah1(2).XTickLabel={'0','1','2','3','4','5','6'};
-ah1(1).XAxis.Label.String={'Model Order'};
-ah1(1).XTickLabel={'0','1','2','3','4','5','6'};
-ah1(1).Title.String={'Post-model';'-BIC/2'};
-set(gcf,'Name','Adapt/Post cross-validation');
-
-%% sqrt data: to check that the long tail of the data does not affect results
-load ../../res/allDataModelsSqrt.mat model datSet
-
-vizDataLikelihood(model(1:9),datSet)
-ph=findobj(gcf,'Type','Axes');
-for i=1:length(ph)
-  set(ph(i),'XTickLabelRotation',0,'XTickLabel',mat2cell(num2str([0:10]'),ones(11,1),2))
+load ../../res/allDataRedAlt_20190510T175706.mat
+%load ../../res/old/allDataRedAlt_20190425T210335.mat %This cointains model estimates up to order 10
+modelAll=modelRed;
+load ../../res/blocked20_CVred_20190403T141344.mat
+mdlBlk20=fitMdlBlocked;
+dsBlk20=datSetBlocked;
+load ../../res/blocked100_CVred_20190425T215717.mat
+mdlBlk100=fitMdlBlocked;
+dsBlk100=datSetBlocked;
+load ../../res/OE_CVred_20190401T195118.mat
+mdlBlk1=fitMdlOE;
+dsBlk1=datSetBlocked;
+load ../../res/AP_CV_20190405T182510.mat
+mdlAP=fitMdlAP;
+dsAP=datSetAP;
+%% Compare relevant metrics for full-data fit
+for i=1:length(modelRed)
+    modelAll{i}.name=num2str(i-1);
 end
+fittedLinsys.compare(modelRed)
+f1=gcf;
+ph=findobj(f1,'Type','Axes');
+for i=1:length(ph)
+   ph(i).XAxis.Label.String='Model order';
+   ph(i).GridLineStyle='none';
+end
+
+%% Compare CV log-L
+mdlList=[mdlBlk1,mdlBlk20,mdlBlk100,[mdlAP;mdlAP(6,:)]];
+namePrefix={'Odd samples','Even samples','Odd blocks [20]','Even blocks [20]','Odd blocks [100]','Even blocks [100]','First half','Second half'};
+for i=1:size(mdlList,1)
+    for j=1:size(mdlList,2)
+        mdlList{i,j}.name=[namePrefix{j} num2str(i-1)];
+        D=mdlList{i,j}.D;
+        D(isnan(D))=0;
+        mdlList{i,j}.D=D;
+    end
+end
+cvDSlist=[dsBlk1([2,1]);dsBlk20([2,1])';dsBlk100([2,1]);dsAP([2,1])];
+mdlList=mdlList(:,1:6); %Ignoring the A/P cross val
+cvDSlist=cvDSlist(1:6);
+[fh] = vizCVDataLikelihood(mdlList,cvDSlist);
+%% Residual plots:
+for i=1:length(modelAll)
+    D=modelAll{i}.D;
+    D(isnan(D))=0;
+    modelAll{i}.D=D;
+end
+    
+f2=linsys.compareResiduals(modelAll,datSet,'det');
+title('Deterministic residuals');
+ax=gca;
+ax.XAxis.TickLabels={'0','1','2','3','4','5','6'};
+ax.YAxis.TickValues=[];
+ax.XAxis.TickLabelRotation=90;
+f3=linsys.compareResiduals(modelAll,datSet,'oneAhead');
+title('One ahead residuals')
+ax.XAxis.TickLabels={'0','1','2','3','4','5','6'};
+
+%% Put everything in single figure:
+f=figure('Units','Pixels','InnerPosition',[100 100 300*4 300*1.5]);
+p1=findobj(f1,'Type','Axes');
+p=copyobj(p1(end-1),f);
+p.Position=[.05 .08 .17 .85];
+p.XAxis.TickValues=[100:100:700];
+p.XAxis.TickLabels=strcat('\color[rgb]{0,0,0}  ', {'0','1','2','3','4','5','6'});
+p.Title.String='BIC';
+p.XAxis.Label.String='';
+p.YAxis.TickValues=[];
+p.YAxis.Limits(1)=0;
+p.XAxis.TickLength=[0 0];
+%Deterministic residuals:
+p1=findobj(f2,'Type','Axes');
+p=copyobj(p1,f);
+p.Position=[.26 .08 .17 .86];
+p.FontSize=8;
+p.Box='off';
+p.XAxis.TickLabels=strcat('\color[rgb]{0,0,0}  ', {'0','1','2','3','4','5','6'});
+p.XAxis.TickLength=[0 0];
+p.Title.String='Det. residuals';
+p.XAxis.Color='w';
+ph=findobj(fh,'Type','Axes');
+ph=copyobj(ph,f);
+ph=ph(end:-1:1);
+for i=1:length(ph)
+   ph(i).Position=[.48+.17*mod(floor((i-1)/2),4) .08+.45*(mod(i,2)) .15 .42];
+   ph(i).YAxis.Label.String=namePrefix{i};
+   if i==3
+   ph(i).Title.String='CV logL';
+   else
+       ph(i).Title.String='';
+   end
+   tt=findobj(ph(i),'Type','text');
+   delete(tt)
+   bb=findobj(ph(i),'Type','bar');
+   if mod(i,2)==0
+       ph(i).XAxis.TickValues=100:100:700;
+       ph(i).XAxis.TickLabels={'0','1','2','3','4','5','6'};
+   else
+       ph(i).XAxis.TickValues=[];
+   end
+   if i==4
+      ph(i).XAxis.Label.String='Model order';
+   end
+end
+
+p=findobj(f,'Type','Axes');
+for i=1:length(p)
+    p(i).YAxis.Color='w';
+    p(i).YAxis.Label.Color='k';
+    p(i).FontName='OpenSans';
+    p(i).FontSize=10;
+    tt=findobj(p(i),'Type','text');
+    set(tt,'FontSize',10)
+end
+saveFig(f,'../../fig/','evaluateOrder',0)
+%%
+linsys.summaryTable([modelAll(4),mdlList(4,:)])
--- src/fit/allDataModelsRedalt_broken.m
+++ src/fit/allDataModelsRedalt_broken.m
@@ -12,7 +12,7 @@ Uf=[U;ones(size(U))];
 datSet=dset(Uf,Yasym');
 
 %% Split dset
-ss=datSet.split(find(Ubreaks));
+ss=datSet.split(find(Ubreaks),true);
 %% Reduce data
 Y=datSet.out;
 U=datSet.in;
@@ -21,58 +21,123 @@ s=var(X'); %Estimate of variance
 flatIdx=s<.005; %Variables are split roughly in half at this threshold
 
 %% Fit Models
-maxOrder=6; %Fitting up to 10 states
+maxOrder=4; %Fitting up to 4 states
 %Opts for indentification:
 opts.robustFlag=false;
 opts.outlierReject=false;
-opts.fastFlag=200;
+opts.fastFlag=false; %Trials are too short to do fast filter
 opts.logFlag=true;
 opts.indD=[];
 opts.indB=1;
-opts.Nreps=20;
+opts.Nreps=5; %Does not matter, all reps converge rapidly to the same solution for order=3
 opts.stableA=true;
+%Refining to get a very precise estimate: needed because each block is
+%constant in terms of inputs, so the difference in the likelihood between
+%a model with outpu y=C*x+D*u and one with y=C*(x+u) +(D-C)*u, comes solely
+%from the dynamics equation, and how well x(k+1)=A*x(k)+B*u(k) compares to
+%x(k+1)+u(k+1)=A*(x(k)+u(k)) +B*u(k), and that difference is only a
+%function of (I-A)*u(k) [u is constant within blocks], which for all large time constants may be a
+%negligible term. Thus, precision matters, a lot.
+%A potential post-hoc fix is to change the states by an arbitrary portion
+%of u in order to enforce continuity at the condition transitions, and see
+%how badly this affects the likelihood. My guess is not much.
+%This also speaks to fitting a constant-term in the inpuit: while in theory
+%possible, there is only a very small difference between a scale with a
+%huge time constant plus a constant offset, and a different constant input
+%contribution. Once again, very precise fitting would be needed, and once
+%again a possible test is to try to force states to be as close to 0 as
+%possible during late baseline or something like that, and see if that
+%affects logL too much.
+opts.refineTol=1e-5;
+opts.refineMaxIter=5e4; %We want a very precise so
 %opts.fixR=median(s(~flatIdx))*eye(size(datSetRed.out,1)); %R proportional to eye
 %opts.fixR=corrMat(~flatIdx,~flatIdx); %Full R
 opts.fixR=[]; %Free R
 opts.includeOutputIdx=find(~flatIdx); 
-[modelRed]=linsys.id(datSet,0:3,opts);
+modelRed{1}='';
+ssPermuted=ss;
+ssPermuted.in=ssPermuted.in([5,4,2,1,3]);
+ssPermuted.out=ssPermuted.out([5,4,2,1,3]);
+[modelRed(2:maxOrder+1)]=linsys.id(ssPermuted,1:maxOrder,opts);
 %% Save (to avoid recomputing in the future)
 nw=datestr(now,'yyyymmddTHHMMSS');
-save(['../../res/allDataRedAltBroken_' nw '.mat'],'modelRed', 'datSet', 'opts');
+save(['../../res/allDataRedAltBroken_' nw '.mat'],'modelRed', 'datSet', 'opts','Ubreaks','flatIdx');
 
-%%
-%load('/Datos/Documentos/code/EMG-LTI-SSM/res/allDataRedAlt_20190425T210335.mat')
-%% For each block, get the smoothed/MLE state estimate
-ord=3;
+%% Compare continuously fitted model and broken model for estimating blocks individually
+clear all
 figure;
-hold on
-x1=0;
-modelRed{ord}=modelRed{ord}.canonize;
-for i=1:length(ss)
-    mleState{i}=modelRed{ord}.Ksmooth(ss{i});
-    if i==1
-        iC=[];
-    else
-        iC=dsstate.getSample(dsstate.Nsamp);
-        iC=mleState{i}.getSample(1);
-    end
-    [dsout,dsstate]=modelRed{ord}.simulate(ss{i}.in,iC,true,true); %Deterministic simulation from last point
-    mleState{i}.plot(x1);
-    hold on
-    %dsstate.plot(x1)
-    %To do: add taus in legend
+load ../../res/allDataRedAlt_20190510T175706.mat %Orders from 0 to 6
+contModel=modelRed; %Model fitted to data as single block
+load ../../res/allDataRedAltBroken_20190616T001216.mat
+blockedModel=modelRed; %Model fitted to individual blocks
+ss=datSet.split(find(Ubreaks),true);
+ord=4; %three-state
 
-    x1=x1+mleState{i}.Nsamp;
+for kk=1:2
+    switch kk
+        case 1
+            %Option 1: load the model fitted to the continous data:
+            mdl=contModel{ord};
+            %ll=modelRed{4}.logL(datSet)
+            %ll=modelRed{4}.logL(ss) %Does not work
+            ttl='Continuous dataset model fit';
+         case 2
+             %Option 2: load the model fitted to the broken data:
+            mdl=blockedModel{ord};
+            %Need to pad,  not sure why it didnt happen at EM time:
+            Dpad=datSet.out/datSet.in;
+            Dpad=Dpad(flatIdx,:);
+            mdl=mdl.pad(flatIdx,Dpad);
+            mdl=mdl.canonize('canonicalAlt');
+            %Shift states by an arbitrary portion of the inputs
+            l1=mdl.logL(ss);
+            K=[[3.3;2.15;0],[0;0;.4]];
+            mdl.D=mdl.D-mdl.C*K;
+            mdl.B=mdl.B-(mdl.A-eye(3))*K;
+            %To Do: linsys function that given K makes this transform
+            %To Do: linsys function to automate K selection to maximize logL of some
+            %target dataset.
+            %Check that shift does not change likelihood:
+            l2=mdl.logL(ss);
+            deltaL=l2-l1 %Should be 0 (numerically)
+            ttl='Blocked dataset model fit';
+    end
+    % For each block, get the smoothed/MLE state estimate
+    subplot(2,1,kk)
+    hold on
+    x1=0;
+    mdl=mdl.canonize('canonicalAlt');
+    for i=1:length(ss.in) %Each block
+        single=ss.extractSingle(i);
+        mleState{i}=mdl.Ksmooth(single);
+        if i==1
+            iC=[];
+        else
+            iC=dsstate.getSample(dsstate.Nsamp); %Last sample of fit to previous block
+            iC=mleState{i}.getSample(1); %First sample of the MLE states
+        end
+        [dsout,dsstate]=mdl.simulate(single.in,iC,true,true); %Deterministic simulation from last point
+        mleState{i}.plot(x1);
+        %dsstate.plot(x1)
+        %To do: add taus in legend
+
+        x1=x1+mleState{i}.Nsamp;
+    end
+    title(ttl)
+    if kk==2
+        xlabel('Strides')
+    end
+    ylabel('State value (a.u.)')
 end
 
-
-%% Compare models
-for i=2:11
+%% Compare model orders for the broken data
+load ../../res/allDataRedAltBroken_20190616T001216.mat
+for i=2:5
 modelRed{i}.name=num2str(i-1);
 end
-fittedLinsys.compare(modelRed(1:11))
+fittedLinsys.compare(modelRed(2:5))
 set(gcf,'Units','Normalized','OuterPosition',[.4 .7 .6 .3])
-saveFig(gcf,'../../fig/','allDataModelsRedAltCompare',0)
+%saveFig(gcf,'../../fig/','allDataModelsRedAltCompare_broken',0)
 %% visualize structure
 modelRed2=cellfun(@(x) x.canonize,modelRed,'UniformOutput',false);
 datSet.vizFit(modelRed2(1:11))
