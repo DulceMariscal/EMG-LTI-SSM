d051fab0ca79ba1981ced8cefc5e09f333334880
DulceMariscal <mariscaldulce@gmail.com>
2022-06-09 14:55:23 -0400

    Update groupDataToMatrixForm.m
    - Adding input for name

--- src/groupDataToMatrixForm.m
+++ src/groupDataToMatrixForm.m
@@ -1,21 +1,28 @@
-function [Y,Yasym,Ycom,U,Ubreaks]=groupDataToMatrixForm(subjIdx,sqrtFlag)
+function [Y,Yasym,Ycom,U,Ubreaks]=groupDataToMatrixForm(subjIdx,sqrtFlag,fName)
 %% Load real data:
-fName='dynamicsData.h5';
+% fName='dynamicsData_C_s12V2.h5';
+% fName='dynamicsData_AUF_s7.h5';
+% fName='dynamicsData_NTS.h5';
+% fName='dynamicsData_CTR.h5';
+% fName='dynamicsData_PATS.h5'
+% fName='dynamicsData_ATR_V4.h5';
+% fName='dynamicsData_ATS_V6.h5';
 EMGdata=h5read(fName,'/EMGdata');
 SLA=h5read(fName,'/SLA');
 speedDiff=h5read(fName,'/speedDiff');
 breaks=h5read(fName,'/breaks');
-labels=hdf5read(fName,'/labels');
+% labels=hdf5read(fName,'/labels');
 
 %%
 U=speedDiff;
 Ubreaks=breaks;
 
 %% Some pre-proc
-if nargin<1
-    subjIdx=2:16; %Excluding C01 only
-end
-muscPhaseIdx=1:360; %All muscles
+% if nargin<1
+%     subjIdx=2:16; %Excluding C01 only
+% end
+%%
+muscPhaseIdx=1:size(EMGdata,2);%336; %14 muscles per leg %360; %All muscles
 Y=EMGdata(:,muscPhaseIdx,subjIdx);
 Y=nanmedian(Y,3); %Median across subjs
 if nargin>1 && sqrtFlag
